7db6f90867dc7558f3804896f5e1a561f81fefa7
Ignacio N. Lucero <igwisnirro@gmail.com>
2015-10-23 10:48:48 +0200

    Desactiva el botón mientras procesa la carga del árbol.

--- src/main/webapp/resources/js/scripts.js
+++ src/main/webapp/resources/js/scripts.js
@@ -291,13 +291,18 @@
 
 
 $('#update-dictionary-button').click(function(){
-	alert( "Recargando el diccionario de conceptos." +
-			"\nPor favor, espere." );
+
+	$(this).prop('disabled', true);
+	
 	$.ajax({
 		url: "api/updateDictionaryCache",
 //		async: false,
 		success: function() {
+			$('#update-dictionary-button').prop('disabled', false);
 			alert( "Diccionario de conceptos recargado." );
 		}
 	});
+	
+	alert( "Recargando el diccionario de conceptos." +
+			"\nPor favor, espere." );
 });
